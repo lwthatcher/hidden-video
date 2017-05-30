607c263369d3680bdfb918009eeea04f620b633f
Lawrence Thatcher <lwthatcher@msn.com>
2017-05-29 18:35:00 -0600

    added video.js dependency (finally...)

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import videojs from 'video.js/dist/video.js';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
-  styleUrls: ['./video.component.css']
+  styleUrls: ['video.component.css']
 })
 export class VideoComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    var player = videojs('my-player');
   }
 
 }
