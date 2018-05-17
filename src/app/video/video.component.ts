8149c02e9f3aade789a219a1973547c388a45042
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-17 13:51:39 -0600

    added canvas element

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import {VgAPI} from 'videogular2/core';
 
 @Component({
@@ -9,16 +9,29 @@ import {VgAPI} from 'videogular2/core';
 export class VideoComponent implements OnInit {
   preload:string = 'auto';
   api:VgAPI;
+  canvas;
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    this.canvas = this.el.nativeElement.querySelector('div.row > div.col-lg > canvas');
     console.log('video component init!', this);
   }
 
   onPlayerReady(api:VgAPI) {
     this.api = api;
     console.log('video player ready', this.api);
+    const w = this.api.videogularElement.clientWidth;
+    const h = this.api.videogularElement.clientHeight;
+    console.log('setting canvas width/height', w, h);
+    this.canvas.width = w;
+    this.canvas.height = h;
+  }
+
+
+  transfer(event) {
+    console.log('clicked transfer', event);
+    console.log('host:', this.el.nativeElement);
   }
 
 }
