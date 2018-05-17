8efea66e6299f814ead687b7ca8ac073c4ca3d02
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-17 14:20:56 -0600

    transfers image to canvas on button press

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import {VgAPI} from 'videogular2/core';
 
+type RenderingContext = CanvasRenderingContext2D;
+
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -9,12 +11,16 @@ import {VgAPI} from 'videogular2/core';
 export class VideoComponent implements OnInit {
   preload:string = 'auto';
   api:VgAPI;
-  canvas;
+  canvas: HTMLCanvasElement;
+  video: HTMLVideoElement;
+  context: RenderingContext;
 
   constructor(private el: ElementRef) { }
 
   ngOnInit() {
     this.canvas = this.el.nativeElement.querySelector('div.row > div.col-lg > canvas');
+    this.video = this.el.nativeElement.querySelector('vg-player > video');
+    this.context = this.canvas.getContext('2d');
     console.log('video component init!', this);
   }
 
@@ -23,15 +29,15 @@ export class VideoComponent implements OnInit {
     console.log('video player ready', this.api);
     const w = this.api.videogularElement.clientWidth;
     const h = this.api.videogularElement.clientHeight;
-    console.log('setting canvas width/height', w, h);
+    console.log('setting canvas width/height', w, h, this.video.clientWidth, this.video.clientHeight);
     this.canvas.width = w;
     this.canvas.height = h;
   }
 
 
   transfer(event) {
-    console.log('clicked transfer', event);
-    console.log('host:', this.el.nativeElement);
+    console.log('transferring image', this.video, this.canvas);
+    this.context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
   }
 
 }
